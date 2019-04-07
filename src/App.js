edebe374342cf474e10535a106b9f219913701c0
Britney Smith <brit.smith7@gmail.com>
2019-04-07 16:57:10 -0400

    Add todo object array for state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Do laundry',
+        completed: false
+      },
+      {
+        id: 1,
+        title: 'Call mom',
+        completed: false
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos />
       </div>
     );
   }
